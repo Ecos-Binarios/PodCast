2086409b33599db3a546ca2c063ae86c191f92c9
Ronaldo Almirón <rony.almiron.015@gmail.com>
2024-05-23 20:37:55 -0300

    fix: Corregir detalles en los estilos CSS de la página

--- assets/modal.js
+++ assets/modal.js
@@ -1,62 +1,77 @@
 
-const modal = document.querySelector(".modal")
-const btnModalOpenLogin = document.querySelector(".btn-login")
-const btnModalOpenSignup = document.querySelector(".btn-signup")
+const modal = document.querySelector(".modal");
+const btnModalOpenLogin = document.querySelector(".btn-login");
+const btnModalOpenSignup = document.querySelector(".btn-signup");
 
-const btnModalCloseSignup = document.querySelector(".signup__header .modal__close")
-const btnModalCloseLogin = document.querySelector(".login__header .modal__close")
+const btnModalCloseSignup = document.querySelector(".signup__header .modal__close");
+const btnModalCloseLogin = document.querySelector(".login__header .modal__close");
 
-const btnOpenLogin = document.querySelector(".signup__login .signup__link")
-const btnOpenSignup = document.querySelector(".login__signup .login__link")
+const btnOpenLogin = document.querySelector(".signup__login .signup__link");
+const btnOpenSignup = document.querySelector(".login__signup .login__link");
 
 const btnOpenLoginFooter = document.getElementById('btnLoginFooter');
 const btnOpenSignupFooter = document.getElementById('btnSignupFooter');
 
-const signup = document.querySelector('.signup')
-const login = document.querySelector('.login')
+const signup = document.querySelector('.signup');
+const login = document.querySelector('.login');
 
 btnModalOpenLogin.addEventListener('click', (e) => {
-    login.style.display = "flex"
+    login.style.display = "flex";
     modal.showModal();
+    document.body.style.overflow = 'hidden';
 })
 
 btnModalOpenSignup.addEventListener('click', (e) => {
-    signup.style.display = "flex"
+    signup.style.display = "flex";
     modal.showModal();
+    document.body.style.overflow = 'hidden';
 })
 
 btnModalCloseSignup.addEventListener('click', (e) => {
-    signup.style.display = "none"
+    signup.style.display = "none";
     modal.close();
+    document.body.style.overflow = 'auto';
 })
 btnModalCloseLogin.addEventListener('click', (e) => {
-    login.style.display = "none"
+    login.style.display = "none";
     modal.close();
+    document.body.style.overflow = 'auto';
 })
 
 btnOpenSignup.addEventListener('click', () => {
-    login.style.display = "none"
-    signup.style.display = "flex"
+    login.style.display = "none";
+    signup.style.display = "flex";
 })
 
 btnOpenLogin.addEventListener('click', () => {
-    signup.style.display = "none"
-    login.style.display = "flex"
+    signup.style.display = "none";
+    login.style.display = "flex";
 })
 
 // FOOTER
 btnOpenSignupFooter.addEventListener('click', (e) => {
     e.preventDefault()
-    login.style.display = "none"
-    signup.style.display = "flex"
-    modal.showModal()
-    console.log("SIgnup")
+    login.style.display = "none";
+    signup.style.display = "flex";
+    modal.showModal();
+    document.body.style.overflow = 'hidden';
 })
 
 btnOpenLoginFooter.addEventListener('click', (e) => {
     e.preventDefault()
-    signup.style.display = "none"
-    login.style.display = "flex"
-    modal.showModal()
-    console.log("LOInf")
+    signup.style.display = "none";
+    login.style.display = "flex";
+    modal.showModal();
+    document.body.style.overflow = 'hidden';
 })
+
+
+// Cierra el modal al hacer clic fuera de él (opcional)
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+        login.style.display = "none";
+        signup.style.display = "none";
+        document.body.style.overflow = 'auto';
+    }
+});
